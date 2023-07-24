3493d397e66f167248bc593c1f42c9498889a810
Bratin Kundu <77373330+bratinkundu-bacancy@users.noreply.github.com>
2023-07-24 12:17:00 +0530

    Debugging in the live change

--- gtm.js
+++ gtm.js
@@ -212,6 +212,7 @@ events.forEach(function (e) {
         }
         if (e === 'load') {
             console.log("reloading...")
+            console.log(window.performance)
             if (window.performance.getEntriesByName('first-contentful-paint').length > 0) {
                 fist_contentful_paint = window.performance.getEntriesByName('first-contentful-paint')[0].startTime;
             }
